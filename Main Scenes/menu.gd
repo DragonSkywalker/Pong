5df827eb3f0f78f09a79a68259d12550c5f147f3
Michael Wu <wurunzhi@hotmail.com>
2023-11-05 18:29:36 +0800

    Added quit button on main menu

--- Main Scenes/menu.gd
+++ Main Scenes/menu.gd
@@ -4,3 +4,6 @@ func _on_play_pressed():
 	Game.ballSpeed = Game.ballInitialSpeed
 	get_tree().change_scene_to_file("res://Main Scenes/score_selection.tscn")
 	
+	
+func _on_quit_pressed():
+	get_tree().quit()

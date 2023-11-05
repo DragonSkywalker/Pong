6071eba2122bdb2b11129299f1191c726f0665bd
Michael Wu <wurunzhi@hotmail.com>
2023-11-05 19:49:57 +0800

    Implemented dashes

--- Global/Game.gd
+++ Global/Game.gd
@@ -8,5 +8,7 @@ var lastLoser = 0
 var ballSpeed = ballInitialSpeed
 const keepSpeedBetweenRounds = false
 const playerSpeed = 500.0
+const dashSpeed = 2000.0
 const ballAcceleration = 10.0
 const ballInitialSpeed = 500.0
+const dashTimes = 5
--- Objects/player_2.gd
+++ Objects/player_2.gd
@@ -5,7 +5,7 @@ var dashing = false
 var dashDirection = 0
 
 func _physics_process(delta):
-	var direction = Input.get_axis("player1_up", "player1_down")
+	var direction = Input.get_axis("player2_up", "player2_down")
 	if dashing:
 		velocity.y = dashDirection * Game.dashSpeed
 	else:
@@ -13,8 +13,8 @@ func _physics_process(delta):
 			velocity.y = direction * Game.playerSpeed
 		else:
 			velocity.y = move_toward(velocity.x, 0, Game.playerSpeed)
-		if Input.is_action_just_pressed("player1_dash") and dashLeft > 0 and get_node("../Ball").hasStarted:
-			dashDirection = Input.get_axis("player1_up", "player1_down")
+		if Input.is_action_just_pressed("player2_dash") and dashLeft > 0 and get_node("../Ball").hasStarted:
+			dashDirection = Input.get_axis("player2_up", "player2_down")
 			if dashDirection:
 				dashing = true
 				dashLeft -= 1

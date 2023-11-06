02693945299fc16adb520fcc2a3b032539f92ac1
Michael Wu <wurunzhi@hotmail.com>
2023-11-06 13:51:56 +0800

    Merged players

--- Objects/player.gd
+++ Objects/player.gd
@@ -3,9 +3,13 @@ extends CharacterBody2D
 var dashLeft = Game.dashTimes
 var dashing = false
 var dashDirection = 0
+var whichP
+
+func _ready():
+	whichP = int(str(name)[-1])
 
 func _physics_process(delta):
-	var direction = Input.get_axis("player1_up", "player1_down")
+	var direction = Input.get_axis("player%d_up" % whichP, "player%d_down" % whichP)
 	if dashing:
 		velocity.y = dashDirection * Game.dashSpeed
 	else:
@@ -13,8 +17,8 @@ func _physics_process(delta):
 			velocity.y = direction * Game.playerSpeed
 		else:
 			velocity.y = move_toward(velocity.x, 0, Game.playerSpeed)
-		if Input.is_action_just_pressed("player1_dash") and dashLeft > 0 and get_node("../Ball").hasStarted:
-			dashDirection = Input.get_axis("player1_up", "player1_down")
+		if Input.is_action_just_pressed("player%d_dash" % whichP) and dashLeft > 0 and get_node("../Ball").hasStarted:
+			dashDirection = Input.get_axis("player%d_up" % whichP, "player%d_down" % whichP)
 			if dashDirection:
 				dashing = true
 				dashLeft -= 1
